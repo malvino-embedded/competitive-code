09951a4451ff86d1611ad303401e3a322724a75b
malvino <malvino@niltech.com>
2025-09-08 19:10:56 +0530

    Print the number 1 to 100 .

---
+++ CPP/Basic/2.Print1to100.cpp
@@ -0,0 +1,9 @@
+//2. Print a number 1 to 100 in loop 
+#include<bits\stdc++.h>
+
+int main(void){
+    for(int i = 1 ; i <=100 ;i++){
+        std::cout << std::setw(2) << i <<std::endl;
+    }     
+    return 0;
+}
