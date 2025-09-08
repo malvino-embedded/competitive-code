1a8d14bd72e17673b20a69f29edec417ad3a1f17
malvino <malvino@niltech.com>
2025-09-08 18:52:45 +0530

    Ignore .exe files

---
+++ CPP/Basic/1.HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Write a code to print the Hello World in console.
+
+#include<bits\stdc++.h>
+
+std::string display(std::string name){
+    return name;
+}
+
+int main(void){
+    std::cout << display("Hello World !!!");
+    return 0;
+}
