a00545e6d29925d77f83bef7d06a5a968e064461
malvino <malvino@niltech.com>
2025-10-06 22:57:16 +0530

    To print the Alphabets form A to Z....

---
+++ CPP/Basic/alphbetsAtoZ.cpp
@@ -0,0 +1,16 @@
+//Print Alphabets form A to Z
+
+#include<bits/stdc++.h>
+
+void alphabets(void){
+	for(char i = 65 ; i <= 90 ; i++){
+		std::cout << i << std::endl;
+	}
+}
+
+int main(void){
+	std::cout << "\n";
+	std::cout << "To Print the Alphabets Letter form A To Z....\n";
+	alphabets();
+	return 0;
+}
