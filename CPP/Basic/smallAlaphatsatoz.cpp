a8ee8b4879418e33e8a0005958d910562b671b5d
malvino <malvino@niltech.com>
2025-10-07 08:09:29 +0530

    To print small Alpha a to z....

---
+++ CPP/Basic/smallAlaphatsatoz.cpp
@@ -0,0 +1,19 @@
+// To write a code print the alphabets form a to z .....
+
+#include<bits/stdc++.h>
+
+
+void alphabets(void){
+	for(char i = 96 ; i < 123 ;i++){
+		std::cout << i << std::endl;
+	}
+}
+
+int main(void){
+	system("cls");
+	std::cout << "Print the Alphabets form a to z ...";
+	alphabets();
+	system("pause");
+	return 0;
+}
+
